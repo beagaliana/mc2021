d1ab0000d4be04bc032dc2440f0e850c3a7aff93
Beatriz Galiana Carballido <beagaliana@riseup.net>
2021-01-05 10:33:51 +0100

    Limpiar HTML y JS
    Reordenadas cieras partes del codigo para mejorar la legibilidad tanto en la
    pagina html como en el propio script

--- index.php
+++ index.php
@@ -2,46 +2,40 @@
 
 <html>
   <head>
-<meta charset="utf-8">
-<title>Sistemas de Funciones Iteradas</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="index.css">
-<link rel="stylesheet" type="text/css" href="index.css">
-<link href="https://fonts.googleapis.com/css?family=
-Roboto|Lato&display=swap" rel="stylesheet">
-<script src="script.js"></script>
-</head>
-<body>  
-
-  <div id="data" style="display:none">
-    <?php include "fractals.html" ?>
-  </div>
-
-
-<h2>Renderizador de Sistemas de Funciones Iteradas</h2>
-<div style="display: flex;">
-  <div id="menu">
-<p style="padding: 5%; font-size: medium;">Elige uno de los siguientes 
-ejemplos clásicos o modifica los parámetros tú mismo:
-<select id="ifs-select"></select></p>
-<p style="text-align: center;">
-<textarea id="values" rows="6" cols="80"></textarea></p>
-<button id="refresh">¡Cargar!</button>
-<p style="padding: 10%; font-size: medium;">Échale un vistazo al 
-<a href="http://www.github.com/beagaliana/mc2021">repositorio</a>
-del código en GitHub
-
-</div>
-<div style="padding-left: 5%;">
-<canvas id="ifs" height="480" width="500" style="outline: rgb(127, 119, 235) 3px solid;">
-    Whoa! Your browser doesn't support canvas?!?!
-</canvas>
-</div>
-</div>
-<script src="script.js"></script>
-
-      </div>
+    <meta charset="utf-8">
+    <title>Sistemas de Funciones Iteradas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link href="https://fonts.googleapis.com/css?family=
+    Roboto|Lato&display=swap" rel="stylesheet">
+  </head>
+  <body>  
+    <div id="data" style="display:none">
+      <?php include "fractals.html" ?>
     </div>
-      </body>
+
+    <h2>Renderizador de Sistemas de Funciones Iteradas</h2>
+    <div style="display: flex;">
+      
+      <div id="menu">
+        <p style="padding: 5%; font-size: medium;">Elige uno de los siguientes ejemplos clásicos o modifica los parámetros tú mismo:
+        <select id="ifs-select"></select></p>
+        <p style="text-align: center;">
+        <textarea id="values" rows="6" cols="80"></textarea></p>
+        <button id="refresh">¡Cargar!</button>
+        <p style="padding: 10%; font-size: medium;">Échale un vistazo al 
+        <a href="http://www.github.com/beagaliana/mc2021">repositorio</a> del código en GitHub
+      </div>
+
+      <div style="padding-left: 5%;">
+          <canvas id="ifs" height="480" width="500" style="outline: rgb(127, 119, 235) 3px solid;">
+          ¡Vaya! Parece que tu navegador no soporta Canvas
+        </canvas>
+      </div>
+      
+      <script src="script.js"></script>
+    </div>
+  </body>
 </html>
 
--- script.js
+++ script.js
@@ -4,7 +4,9 @@
  * each of which is an array of the six coefficients a, b, c, d, e, f.
  */
 
-(function () {
+
+
+function main() {
 
     var canvas = document.getElementById("ifs"),
         width = canvas.width,
@@ -23,136 +25,123 @@
                   [255, 0, 102],[153, 0, 153],[0,0,0]];
         needRefresh = false;
 
-        /*
-         * Reads the values from the textarea into the matrix.
-         */
-        readIfs = function () {
-            values = document.getElementById("values").value.trim().split(/\s+/);
-            // Transforma tipo texto en tipo NaN
-            for (var i = 0; i < values.length; i++) values[i]=Number(values[i]);
-            if (!(values.length > 0 && values.length % 7 == 0)) {
-                alert("Invalid IFS");
-            } else {
-                fractal = [];
-                for (var i = 0; i < values.length; i += 7) {
-                    fractal.push(values.slice(i, i + 7));
-                }
+    /* Reads the values from the textarea into the matrix. */
+    function readIfs() {
+        values = document.getElementById("values").value.trim().split(/\s+/);
+        // Transforma tipo texto en tipo NaN
+        for (var i = 0; i < values.length; i++) values[i]=Number(values[i]);
+        if (!(values.length > 0 && values.length % 7 == 0)) {
+            alert("Invalid IFS");
+        } else {
+            fractal = [];
+            for (var i = 0; i < values.length; i += 7) {
+                fractal.push(values.slice(i, i + 7));
             }
-        },
+        }
+    }
 
-        /*
-         * Plots a point (in world coordinates) into the pixel array.
-         */
-        plot = function (c) {
-            var px = scaleX * x + offsetX,
-                py = scaleY * y + offsetY,
-                base = (Math.floor(py) * width + Math.floor(px))*4;
+    /* Plots a point (in world coordinates) into the pixel array. */
+    function plot(c) {
+        var px = scaleX * x + offsetX,
+            py = scaleY * y + offsetY,
+            base = (Math.floor(py) * width + Math.floor(px))*4;
 
-            if(!c) elige_color_probabilidad=colors[0]; else elige_color_probabilidad = colors[c % 8];
+        if(!c) elige_color_probabilidad=colors[0]; else elige_color_probabilidad = colors[c % 8];
 
-            pixels[base] = elige_color_probabilidad[0];
-            pixels[base+1] = elige_color_probabilidad[1];
-            pixels[base+2] = elige_color_probabilidad[2];
-            pixels[base+3] = 255;
+        pixels[base] = elige_color_probabilidad[0];
+        pixels[base+1] = elige_color_probabilidad[1];
+        pixels[base+2] = elige_color_probabilidad[2];
+        pixels[base+3] = 255;
 
-        },
+    }
         
 
-        /*
-         * Updates (x,y) to another value in the fractal, by selecting a transform at random
-         * and applying it.
-         */
-        next = function () {
-            var r = Math.random(), probabilityThreshold = 0.0;
-            for (var i = 0; i < fractal.length; i += 1) {
-                var t = fractal[i];
-                if (r <= (probabilityThreshold += t[6])) {
-                    var oldx = x;
-                    // Nuevos valores de X e Y
-                    x = t[0] * x + t[1] * y + t[4];
-                    y = t[2] * oldx + t[3] * y + t[5];
-                    return i;  // Rompemos el bucle y almacenamos indice para seleccionar color en plot
-                }
+    /* Updates (x,y) to another value in the fractal, by selecting a transform at random and applying it. */
+    function next() {
+        var r = Math.random(), probabilityThreshold = 0.0;
+        for (var i = 0; i < fractal.length; i += 1) {
+            var t = fractal[i];
+            if (r <= (probabilityThreshold += t[6])) {
+                var oldx = x;
+                // Nuevos valores de X e Y
+                x = t[0] * x + t[1] * y + t[4];
+                y = t[2] * oldx + t[3] * y + t[5];
+                return i;  // Rompemos el bucle y almacenamos indice para seleccionar color en plot
             }
-        },
+        }
+    }
 
-        findBounds = function () {
-            var left = 0, right = 0, top = 0, bottom = 0;
-            x = 0;
-            y = 0;
+    function findBounds() {
+        var left = 0, right = 0, top = 0, bottom = 0;
+        x = 0;
+        y = 0;
 
-            // Bounce around hoping to settle on the bounding box
-            for (var i = 0; i < 10000; i += 1) {
-                next();
-                // Reasignar limites del canvas
-                if (x < left) left = x;
-                if (x > right) right = x;
-                if (y < bottom) bottom = y;
-                if (y > top) top = y;
-            }
+        // Bounce around hoping to settle on the bounding box
+        for (var i = 0; i < 10000; i += 1) {
+            next();
+            // Reasignar limites del canvas
+            if (x < left) left = x;
+            if (x > right) right = x;
+            if (y < bottom) bottom = y;
+            if (y > top) top = y;
+        }
 
-            // Adjust for a 1:1 aspect ratio.
-            if (top - bottom > right - left) {
-                left = (left + right - top + bottom) / 2;
-                right = left + top - bottom;
-            } else {
-                bottom = (bottom + top - right + left) / 2;
-                top = bottom + right - left;
-            }
+        // Adjust for a 1:1 aspect ratio.
+        if (top - bottom > right - left) {
+            left = (left + right - top + bottom) / 2;
+            right = left + top - bottom;
+        } else {
+            bottom = (bottom + top - right + left) / 2;
+            top = bottom + right - left;
+        }
 
-            // Cuadra elementos dentro del canvas
-            scaleX = width / (right - left);
-            scaleY = height / (bottom - top);
-            offsetX = (width * left) / (left - right);
-            offsetY = (height * top) / (top - bottom);
-        },
+        // Cuadra elementos dentro del canvas
+        scaleX = width / (right - left);
+        scaleY = height / (bottom - top);
+        offsetX = (width * left) / (left - right);
+        offsetY = (height * top) / (top - bottom);
+    }
 
-        /*
-         * Clears the pixel buffer then draws the current fractal, whatever it may be,
-         */
-        newPicture = function () {
-            // Clear pixel data - Colorea el fondo
-            for (var i = 0; i < width * height * 4; i += 4) {
-                pixels[i] = 0; //R
-                pixels[i+1] = 0; //G
-                pixels[i+2] = 0; //B
-                pixels[i+3] = 0; //T
-            }
-            ctx.putImageData(imageData, 0, 0);
+    /* Clears the pixel buffer then draws the current fractal, whatever it may be */
+    function newPicture(){
+        // Clear pixel data - Colorea el fondo
+        for (var i = 0; i < width * height * 4; i += 4) {
+            pixels[i] = 0; //R
+            pixels[i+1] = 0; //G
+            pixels[i+2] = 0; //B
+            pixels[i+3] = 0; //T
+        }
+        ctx.putImageData(imageData, 0, 0);
 
-            readIfs();
+        readIfs();
 
-            // Determine how to orient the picture
-            findBounds();
-        },
+        // Determine how to orient the picture
+        findBounds();
+    }
 
-        /**
-         * MAIN METHOD 
-         * 
-         * Draws a burst of points, then schedules itself to run again.
-         */
-        drawSomePoints = function () {
-            if (needRefresh) {
-                newPicture();
-                needRefresh = false;
-            }
+    /* Draws a burst of points, then schedules itself to run again. */
+    function drawSomePoints() {
+        if (needRefresh) {
+            newPicture();
+            needRefresh = false;
+        }
 
-            for (var i = 0; i < 500; i += 1) {
-                var c = next();
-                plot(c);
-            }
+        for (var i = 0; i < 500; i += 1) {
+            var c = next();
+            plot(c);
+        }
 
-            // Dump the entire pixel data array to the canvas
-            ctx.putImageData(imageData, 0, 0);
+        // Dump the entire pixel data array to the canvas
+        ctx.putImageData(imageData, 0, 0);
 
-            setTimeout(drawSomePoints, 500);
-        },
+        setTimeout(drawSomePoints, 500);
+    }
 
 
-        select = document.getElementById("ifs-select"), // Desplegable fractales
-        values = document.getElementById("values"), // Cuadro de texto con valores del IFS
-        refresh = document.getElementById("refresh"), // boton cargar
-        data = document.getElementById("data").childNodes; // divs conteniendo diferentes fractales
+    select = document.getElementById("ifs-select"), // Desplegable fractales
+    values = document.getElementById("values"), // Cuadro de texto con valores del IFS
+    refresh = document.getElementById("refresh"), // boton cargar
+    data = document.getElementById("data").childNodes; // divs conteniendo diferentes fractales
 
 
     // Creates select dropdown
@@ -177,5 +166,7 @@
     // Draw a burst of points every 500ms.
     setTimeout(drawSomePoints, 500);
 
-}());
+}
+
+main();
 
